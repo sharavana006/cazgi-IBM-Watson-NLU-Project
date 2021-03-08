7696003981166d4395fff22b874d7ac8281c1f94
sharavana006 <sharavana006@gmail.com>
2021-03-08 09:16:58 +0000

    server changes implemented

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = new express();
+const dotenv=require('dotenv');
+dotenv.config();
 
 app.use(express.static('client'))
 
@@ -31,3 +33,26 @@ let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+function getNLUInstance() {
+let api_key = process.env.API_KEY; 
+let api_url = process.env.API_URL;
+const NaturallanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const naturalLanguageUnderstanding = new NaturallanguageUnderstandingV1({
+
+version: '2020-08-01',
+
+authenticator: new IamAuthenticator({
+
+apikey: api_key,
+
+}),
+
+serviceUrl: api_url,
+
+});
+
+return naturalLanguageUnderstanding;
+
+}
